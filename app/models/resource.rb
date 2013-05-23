dcfff89abd3de213755fd61e08810bfb84136bf8
Brooks Swinnerton <bs122@nyu.edu>
2013-05-23 12:22:50 -0400

    Adding support for dynamic fields

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -4,4 +4,18 @@ class Resource
   field :host, type: String
   field :url, type: String
   field :domain, type: String
+
+  def self.normalize_additional_fields(params)
+    normalized_params = {}
+    normalized_params[params[:column_name]] = params[:column_value]
+    params.delete(:column_name)
+    params.delete(:column_value)
+    params.each do |name, value|
+      unless name == params[:column_name]
+        normalized_params[name] = value      
+      end
+    end
+    normalized_params
+  end
+
 end
