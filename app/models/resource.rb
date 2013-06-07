e2b5e3993c96b851ee1100e1ec96863866455707
Brooks Swinnerton <bswinnerton@gmail.com>
2013-06-07 12:14:24 -0400

    Beginning of support for #3

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,22 +1,23 @@
 class Resource
   include Mongoid::Document
-  field :title, type: String
-  field :host, type: String
-  field :url, type: String
-  field :domain, type: String
 
   def self.normalize_additional_fields(params)
     normalized_params = {}
+
     params[:dynamic_field].each do |dynamic_field|
       next if dynamic_field[:name].empty?
       normalized_params[dynamic_field[:name]] = dynamic_field[:value]
     end
     params.delete(:dynamic_field)
+
     params.each do |name, value|
-      unless name == params[:column_name]
+      if normalized_params.keys.include? name
+        normalized_params[name] = [normalized_params[name], name]
+      else
         normalized_params[name] = value      
       end
     end
+
     normalized_params
   end
 
